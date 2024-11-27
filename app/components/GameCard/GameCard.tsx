4c9991c8421981be74c5191058785a1008176f60
sharmaryan <panditaryan.as@gmail.com>
2024-11-27 20:23:16 +0530

    styles : game card

--- app/components/GameCard/GameCard.tsx
+++ app/components/GameCard/GameCard.tsx
@@ -16,7 +16,11 @@ export const GameCard = ({
       <p className="text-secondary text-sm md:text-base xl:text-lg xl:font-bold">
         {name.en}
       </p>
-      <Link href={url ?? "#"} className="relative h-20 w-full md:h-40 xl:h-60" aria-label="game image">
+      <Link
+        href={url ?? "#"}
+        className="relative h-20 w-full md:h-40 xl:h-60"
+        aria-label="game image"
+      >
         <Image
           src={assets.cover}
           alt=""
@@ -30,26 +34,35 @@ export const GameCard = ({
       <div className="flex mt-auto">
         {gamePreviews?.en && (
           <Tooltip tooltipText="Watch Video">
-            <Link href={gamePreviews.en} className="text-secondary" aria-label="youtube">
+            <Link
+              href={gamePreviews.en}
+              className="text-secondary"
+              aria-label="youtube"
+            >
               <div>
                 <FaYoutube size={15} className="text-warning md:hidden" />
-                <FaYoutube size={20} className="text-warning hidden md:block xl:hidden" />
+                <FaYoutube
+                  size={20}
+                  className="text-warning hidden md:block xl:hidden"
+                />
                 <FaYoutube size={30} className="text-warning hidden xl:block" />
               </div>
             </Link>
           </Tooltip>
         )}
         <div className="ml-auto">
-
-        {url && (
-          <Tooltip tooltipText="Play">
-            <Link href={url} className="text-secondary" aria-label="gaming">
-              <FaGamepad size={15} className="text-success md:hidden" />
-              <FaGamepad size={20} className="text-success hidden md:block xl:hidden" />
-              <FaGamepad size={30} className="text-success hidden xl:block" />
-            </Link>
-          </Tooltip>
-        )}
+          {url && (
+            <Tooltip tooltipText="Play">
+              <Link href={url} className="text-secondary" aria-label="gaming">
+                <FaGamepad size={15} className="text-success md:hidden" />
+                <FaGamepad
+                  size={20}
+                  className="text-success hidden md:block xl:hidden"
+                />
+                <FaGamepad size={30} className="text-success hidden xl:block" />
+              </Link>
+            </Tooltip>
+          )}
         </div>
       </div>
     </div>
