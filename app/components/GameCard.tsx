90e37ae15b4d07262da21229626b6e49ece415c2
sharmaryan <panditaryan.as@gmail.com>
2024-11-27 12:36:42 +0530

    feat : added tootip in Gamecard

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import { GameCardProps } from "../types/gameCard";
 import { BLUR_IMAGE_URL } from "../lib/blurImage";
+import { FaGamepad, FaYoutube } from "react-icons/fa";
+import Link from "next/link";
+import Tooltip from "./Tooltip/Tooltip";
 
 export const GameCard = ({
   assets,
   name,
-  description,
   tags,
+  gamePreviews,
+  url,
 }: GameCardProps) => {
   return (
     <div className="flex flex-col border-2 border-primary px-6 py-4 rounded-xl w-80 gap-5 bg-background_primary">
       <p className="text-secondary text-lg font-bold">{name.en}</p>
-      <div className="relative h-60 w-full">
+      <Link href={url ?? '#'} className="relative h-60 w-full">
         <Image
           src={assets.cover}
           alt=""
@@ -21,13 +25,33 @@ export const GameCard = ({
           placeholder="blur"
           blurDataURL={BLUR_IMAGE_URL}
         />
-      </div>
+      </Link>
       <div className="flex flex-wrap gap-2">
         {tags?.en.map((tag) => (
-          <p key={tag} className="bg-secondary rounded-lg text-primary text-lg font-bold p-2">{tag}</p>
+          <p
+            key={tag}
+            className="bg-secondary rounded-lg text-primary text-lg font-bold p-2"
+          >
+            {tag}
+          </p>
         ))}
       </div>
-      {/* <p className="text-secondary text-lg font-light">{description?.en}</p> */}
+      <div className="flex justify-between">
+        {gamePreviews?.en && (
+          <Tooltip tooltipText="Watch Video">
+            <Link href={gamePreviews.en} className="text-secondary">
+              <FaYoutube size={25} />
+            </Link>
+          </Tooltip>
+        )}
+        {url && (
+          <Tooltip tooltipText="Play">
+            <Link href={url} className="text-secondary">
+              <FaGamepad size={25} />
+            </Link>
+          </Tooltip>
+        )}
+      </div>
     </div>
   );
 };
