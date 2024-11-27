365af11265ada2eae670ec3e5284dc2c13d13321
sharmaryan <panditaryan.as@gmail.com>
2024-11-27 12:51:33 +0530

    refactor : moved types to separate folder

---
+++ app/components/GameCardContainer/GameCardContainer.types.ts
@@ -0,0 +1,5 @@
+export type GameCardContainerProps = {
+    children: React.ReactNode;
+    categoryName: string;
+    icon: string
+}
---
+++ app/components/GamesList/GamesList.types.ts
@@ -0,0 +1,7 @@
+import { GroupedGames } from "../../types/filterGames";
+
+export type GamesListProps = {
+    games: GroupedGames;
+    category: string;
+    icon: string;
+}
