d07882f7898a0f3b5863e6bc78f6e33053e40b3b
sharmaryan <panditaryan.as@gmail.com>
2024-11-28 16:49:21 +0530

    feat : added pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
   title: "Gamezop",
   description:
     "On Gamezop you can play the best free online games. Gamezop has the best online games selection of multiplayer games and single-player games",
+  generator: "Next.js",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/favicon-512.png" },
+    { rel: "icon", url: "icons/favicon-512.png" },
+  ],
+  manifest: "/manifest.json",
+  keywords: ["nextjs13", "pwa", "next-pwa"],
+  authors: [{ name: "Aryan Sharma" }],
 };
 
 export default function RootLayout({
@@ -27,6 +35,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="icons/favicon-512.png" />
+        <link rel="apple-touch-icon" href="icons/favicon-512.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
